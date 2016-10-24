87500a7eb1083cdc17168e58e9ec6d79f60ba3ca
Qingfeng Song <songqingfeng@picb.ac.cn>
2016-10-24 15:06:51 +0800

    add weather.java

---
+++ model/Weather.java
@@ -0,0 +1,12 @@
+package model;
+
+
+//for storing the data
+public class Weather {
+	
+	int start_day = 1; // default is the first day. 
+	public double PPFD [] = new double[8800];
+	public double Temperature [] = new double[8800];
+	public double RH [] = new double[8800];
+
+}
